fdcbb5e80743834473d8d80606f42ab0ce6ec3d6
RodolfoViegas <rodolfo.viegas2@gmail.com>
2022-05-11 22:28:44 -0300

    Há algo de errado na regra

--- jogo_da_vida.py
+++ jogo_da_vida.py
@@ -14,18 +14,24 @@ grid = np.zeros((tam_matriz,tam_matriz))
 
 #Popula a matriz com celulas vivas alocadas de modo aleatório
 for i in range(individuos):
-    linha = np.random.randint(low=0,high=passos-1)
-    coluna = np.random.randint(low=0,high=passos-1)
+    linha = np.random.randint(low=0,high=(passos*2)-1)
+    coluna = np.random.randint(low=0,high=(passos*2)-1)
     grid[linha][coluna] = 1
 
-for i in range(tam_matriz):
-    for j in range(tam_matriz):
-        if (grid[i][j]==0 and (grid[i][j-1] + grid[i][j+1] + grid[i+1][j] + grid[i-1][j] + grid[i+1][j+1] + grid[i+1][j-1] + grid[i-1][j-1] + grid[i-1][j+1] == 3)):
-            grid[i][j] = 1
-        elif (grid[i][j]==1 and ((grid[i][j-1] + grid[i][j+1] + grid[i+1][j] + grid[i-1][j] + grid[i+1][j+1] + grid[i+1][j-1] + grid[i-1][j-1] + grid[i-1][j+1] == 2) or 
-        (grid[i][j-1] + grid[i][j+1] + grid[i+1][j] + grid[i-1][j] + grid[i+1][j+1] + grid[i+1][j-1] + grid[i-1][j-1] + grid[i-1][j+1] == 3))):
-            grid[i][j] = 1
 
-        else:
-            grid[i][j] = 0
 
+# A regra de atualização da celula está quase pronta,falta ainda testar um modo de
+for k in range(passos): 
+    for i in range(tam_matriz):
+        for j in range(tam_matriz):
+            if (grid[i][j]==0 and (grid[i][j-1] + grid[i][(j+1) % len(grid[i])] + grid[(i+1) % len(grid[i])][j] + grid[i-1][j] + grid[(i+1) % len(grid[i])][(j+1) % len(grid[i])] + grid[(i+1) % len(grid[i])][j-1] + grid[i-1][j-1] + grid[i-1][(j+1) % len(grid[i])] == 3)):
+                grid[i][j] = 1
+            elif (grid[i][j]==1 and ((grid[i][j-1] + grid[i][(j+1) % len(grid[i])] + grid[(i+1) % len(grid[i])][j] + grid[i-1][j] + grid[(i+1) % len(grid[i])][(j+1) % len(grid[i])] + grid[(i+1) % len(grid[i])][j-1] + grid[i-1][j-1] + grid[i-1][(j+1) % len(grid[i])] == 2) or 
+            (grid[i][j-1] + grid[i][(j+1) % len(grid[i])] + grid[(i+1) % len(grid[i])][j] + grid[i-1][j] + grid[(i+1) % len(grid[i])][(j+1) % len(grid[i])] + grid[(i+1) % len(grid[i])][j-1] + grid[i-1][j-1] + grid[i-1][(j+1) % len(grid[i])] == 3))):
+                grid[i][j] = 1
+
+            else:
+                grid[i][j] = 0
+    plt.imshow(grid, cmap='Greys')
+    plt.pause(0.0001)
+plt.show()
